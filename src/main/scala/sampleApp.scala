7cd0e329c2a1b44bda883e9a71325cc5516d973d
techroad <mbravi@gmail.com>
2017-11-02 12:42:20 +0530

    Update sampleApp.scala

--- src/main/scala/sampleApp.scala
+++ src/main/scala/sampleApp.scala
@@ -1,18 +1,25 @@
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
 object sampleApp {
 
   def main(args:Array[String]): Unit ={
 
-  val conf = new SparkConf().setMaster("local[2]").setAppName("Sample")
+  //val conf = new SparkConf()
 
-    val sc = new SparkContext(conf)
+    val spark = SparkSession
+      .builder
+      .appName("Sample APP")
+      .getOrCreate()
+
+      //  val sc = new SparkContext(conf)
 
     val data = List("marriott hotel", "hotel marriott")
 
-    sc.parallelize(data).flatMap(x => x.split(" ")).map(x => (x,1)).reduceByKey((x,y) => x+y).foreach(println)
+    spark.sparkContext.parallelize(data).flatMap(x => x.split(" ")).map(x => (x,1)).reduceByKey((x,y) => x+y).foreach(println)
 
-    //
+      //
+    
   }
 
 }
